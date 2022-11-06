201eb0ecf108aa7aeb5339ffdbe800e3e433f013
davidcode10 <davidedson2030@outlook.com>
2022-11-06 16:07:43 +0100

    Initial Commit

---
+++ SequencedAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SequencedAnimation
+//
+//  Created by David Santiago on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
